f27a41050b4357a79b098aad5e6ee2320b0823cd
Raphael Castro <2117949+rafaph@users.noreply.github.com>
2024-05-27 13:37:10 -0300

    feat: remove homebrew feature

--- src/base-ubuntu/.devcontainer/post-install/install.sh
+++ src/base-ubuntu/.devcontainer/post-install/install.sh
@@ -8,8 +8,3 @@ sed -i '/^ZSH_THEME/c\ZSH_THEME="agnoster"' /home/vscode/.zshrc
 # set default user
 echo -e "DEFAULT_USER=vscode\n" >> /home/vscode/.zshrc
 
-# install brew packages
-cd "$(brew --repository)" && git fetch && git reset --hard origin/master && cd -
-brew install bat
-brew install bat-extras
-brew install eza
